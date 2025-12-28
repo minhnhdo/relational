ee8aa9f0884456b8ab905b5a8fa4443844cc5c91
Matthew Do <qvs82v2vm@mozmail.com>
2025-12-28 14:46:31 -0500

    added support for compounded select statements

--- query/src/ast.rs
+++ query/src/ast.rs
@@ -16,6 +16,32 @@ pub enum ResultColumn {
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct SelectStatement {
+pub struct SelectCore {
     pub result_columns: Vec<ResultColumn>,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum CompoundOperator {
+    Union,
+    UnionAll,
+    Intersect,
+    Except,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum SelectStatementCore {
+    Simple(Box<SelectCore>),
+    Compounded(Box<CompoundedSelectCores>),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct CompoundedSelectCores {
+    pub lhs: SelectStatementCore,
+    pub operator: CompoundOperator,
+    pub rhs: SelectCore,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct SelectStatement {
+    pub core: SelectStatementCore,
 }
--- query/tests/select_statement_unit_tests.rs
+++ query/tests/select_statement_unit_tests.rs
@@ -1,4 +1,7 @@
-use query::ast::{Expression, Literal, ResultColumn, SelectStatement};
+use query::ast::{
+    CompoundOperator, CompoundedSelectCores, Expression, Literal, ResultColumn, SelectCore,
+    SelectStatement, SelectStatementCore,
+};
 
 #[test]
 fn simple_select() {
@@ -8,9 +11,11 @@ fn simple_select() {
     assert_eq!(
         value,
         SelectStatement {
-            result_columns: vec![ResultColumn::Expression(Expression::Literal(
-                Literal::String("foo".to_string())
-            ))]
+            core: SelectStatementCore::Simple(Box::new(SelectCore {
+                result_columns: vec![ResultColumn::Expression(Expression::Literal(
+                    Literal::String("foo".to_string())
+                ))]
+            }))
         }
     );
 }
@@ -23,9 +28,11 @@ fn select_concatenated_string() {
     assert_eq!(
         value,
         SelectStatement {
-            result_columns: vec![ResultColumn::Expression(Expression::Literal(
-                Literal::String("foobar".to_string())
-            ))]
+            core: SelectStatementCore::Simple(Box::new(SelectCore {
+                result_columns: vec![ResultColumn::Expression(Expression::Literal(
+                    Literal::String("foobar".to_string())
+                ))]
+            }))
         }
     );
 }
@@ -38,10 +45,41 @@ fn select_multiple_expressions() {
     assert_eq!(
         value,
         SelectStatement {
-            result_columns: vec![
-                ResultColumn::Expression(Expression::Literal(Literal::String("foo".to_string()))),
-                ResultColumn::Expression(Expression::Literal(Literal::String("bar".to_string())))
-            ]
+            core: SelectStatementCore::Simple(Box::new(SelectCore {
+                result_columns: vec![
+                    ResultColumn::Expression(Expression::Literal(Literal::String(
+                        "foo".to_string()
+                    ))),
+                    ResultColumn::Expression(Expression::Literal(Literal::String(
+                        "bar".to_string()
+                    )))
+                ]
+            }))
         }
     );
+}
+
+#[test]
+fn select_union_select() {
+    let value = query::grammar::SelectStatementParser::new()
+        .parse("SELECT 'foo' UNION SELECT 'bar'")
+        .unwrap();
+    assert_eq!(
+        value,
+        SelectStatement {
+            core: SelectStatementCore::Compounded(Box::new(CompoundedSelectCores {
+                lhs: SelectStatementCore::Simple(Box::new(SelectCore {
+                    result_columns: vec![ResultColumn::Expression(Expression::Literal(
+                        Literal::String("foo".to_string())
+                    ))]
+                })),
+                operator: CompoundOperator::Union,
+                rhs: SelectCore {
+                    result_columns: vec![ResultColumn::Expression(Expression::Literal(
+                        Literal::String("bar".to_string())
+                    ))]
+                }
+            }))
+        }
+    )
 }
